e7ae2dbbe706bcf12fab4144710fc98d8f776aed
Humberto Cezar <skateforevis@hotmail.com>
2021-08-27 21:22:39 -0300

    starting project

--- src/main/java/com/byow/wallet/byow/ByowApplication.java
+++ src/main/java/com/byow/wallet/byow/ByowApplication.java
@@ -1,13 +1,13 @@
 package com.byow.wallet.byow;
 
-import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ByowApplication {
 
 	public static void main(String[] args) {
-		Application.launch(GuiApplication.class, args);
+		SpringApplication.run(ByowApplication.class, args);
 	}
 
 }
