01d1b84626fdd1a6299585b2689902d62e5e7c21
Humberto Cezar <skateforevis@hotmail.com>
2021-10-24 22:43:36 -0300

    creating wallet part 8

--- src/main/java/com/byow/wallet/byow/gui/controllers/ReceiveTabController.java
+++ src/main/java/com/byow/wallet/byow/gui/controllers/ReceiveTabController.java
@@ -1,7 +1,10 @@
 package com.byow.wallet.byow.gui.controllers;
 
+import com.byow.wallet.byow.observables.CurrentWallet;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
@@ -11,10 +14,17 @@ import java.io.IOException;
 
 @Component
 public class ReceiveTabController extends Tab {
+    private final CurrentWallet currentWallet;
+
+    @FXML
+    private TextField receivingAddress;
+
     public ReceiveTabController(
         @Value("fxml/receive_tab.fxml") Resource fxml,
-        ApplicationContext context
+        ApplicationContext context,
+        CurrentWallet currentWallet
     ) throws IOException {
+        this.currentWallet = currentWallet;
         FXMLLoader fxmlLoader = new FXMLLoader(
             fxml.getURL(),
             null,
@@ -24,5 +34,11 @@ public class ReceiveTabController extends Tab {
         fxmlLoader.setController(this);
         fxmlLoader.setRoot(this);
         fxmlLoader.load();
+    }
+
+    public void initialize() {
+        currentWallet.receivingAddressProperty().addListener((observable, oldValue, newValue) ->
+            receivingAddress.setText(newValue)
+        );
     }
 }
