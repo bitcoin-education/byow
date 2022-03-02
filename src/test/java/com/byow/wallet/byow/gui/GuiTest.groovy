e330a0886a14b8ba8829d77c88659cb01349319d
Humberto Cezar <skateforevis@hotmail.com>
2022-03-02 19:40:11 -0300

    receiving block part 1 real

--- src/test/java/com/byow/wallet/byow/gui/GuiTest.groovy
+++ src/test/java/com/byow/wallet/byow/gui/GuiTest.groovy
@@ -1,6 +1,19 @@
 package com.byow.wallet.byow.gui
 
+import com.byow.wallet.byow.api.services.ExtendedPubkeyService
+import com.byow.wallet.byow.api.services.SegwitAddressGenerator
+import com.byow.wallet.byow.api.services.node.NodeLoadOrCreateWalletService
+import com.byow.wallet.byow.api.services.node.client.NodeGenerateToAddressClient
+import com.byow.wallet.byow.api.services.node.client.NodeGetBalanceClient
+import com.byow.wallet.byow.api.services.node.client.NodeGetNewAddressClient
+import com.byow.wallet.byow.api.services.node.client.NodeSendToAddressClient
+import com.byow.wallet.byow.domains.AddressType
 import com.byow.wallet.byow.gui.events.GuiStartedEvent
+import io.github.bitcoineducation.bitcoinjava.ExtendedKeyPrefixes
+import io.github.bitcoineducation.bitcoinjava.ExtendedPrivateKey
+import io.github.bitcoineducation.bitcoinjava.ExtendedPubkey
+import io.github.bitcoineducation.bitcoinjava.MnemonicSeed
+import javafx.scene.control.TableView
 import javafx.stage.Stage
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,9 +25,37 @@ import org.testfx.framework.spock.ApplicationSpec
 
 import java.security.Security
 
+import static java.util.concurrent.TimeUnit.SECONDS
+import static org.testfx.util.WaitForAsyncUtils.waitFor
+
 @SpringBootTest
 @ActiveProfiles('test')
 abstract class GuiTest extends ApplicationSpec {
+
+    public static final String TESTWALLET = "testwallet"
+
+    public static final int TIMEOUT = 10
+
+    @Autowired
+    protected NodeSendToAddressClient nodeSendToAddressClient
+
+    @Autowired
+    protected NodeLoadOrCreateWalletService nodeLoadOrCreateWalletService
+
+    @Autowired
+    protected NodeGetBalanceClient nodeGetBalanceClient
+
+    @Autowired
+    protected NodeGetNewAddressClient nodeGetNewAddressClient
+
+    @Autowired
+    protected NodeGenerateToAddressClient nodeGenerateToAddressClient
+
+    @Autowired
+    protected ExtendedPubkeyService extendedPubkeyService
+
+    @Autowired
+    protected SegwitAddressGenerator segwitAddressGenerator
 
     @Autowired
     protected ApplicationContext context
@@ -38,4 +79,33 @@ abstract class GuiTest extends ApplicationSpec {
         this.context.publishEvent(new GuiStartedEvent(this, stage));
     }
 
+    def loadWalletAndAddBalance() {
+        nodeLoadOrCreateWalletService.loadOrCreateWallet(TESTWALLET)
+        createBalanceIfNecessary()
+    }
+
+    protected void sendBitcoinAndWait(String address, String expectedTotalAmount = "1.0", int expectedTotalSize = 1) {
+        nodeSendToAddressClient.sendToAddress(TESTWALLET, address, 1.0)
+        waitFor(TIMEOUT, SECONDS, {
+            TableView tableView = lookup("#addressesTable").queryAs(TableView)
+            return tableView.items.size() == expectedTotalSize && tableView.items[expectedTotalSize - 1].balance == expectedTotalAmount
+        })
+    }
+
+    protected void createBalanceIfNecessary() {
+        double balance = nodeGetBalanceClient.get(TESTWALLET)
+        if (balance < 50) {
+            String address = nodeGetNewAddressClient.getNewAddress(TESTWALLET)
+            nodeGenerateToAddressClient.generateToAddress(TESTWALLET, 150, address)
+        }
+    }
+
+   protected boolean addressIsValid(String address, String mnemonicSeedString, Integer index) {
+        MnemonicSeed mnemonicSeed = new MnemonicSeed(mnemonicSeedString)
+        ExtendedPrivateKey masterKey = mnemonicSeed.toMasterKey("", ExtendedKeyPrefixes.MAINNET_PREFIX.getPrivatePrefix())
+        String extendedPubkeyString = extendedPubkeyService.create(masterKey, "84'/0'/0'/0/".concat(index.toString()), AddressType.SEGWIT).getKey()
+        ExtendedPubkey extendedPubkey = ExtendedPubkey.unserialize(extendedPubkeyString)
+        String expectedAddress = segwitAddressGenerator.generate(extendedPubkey)
+        return expectedAddress == address
+    }
 }
---
+++ src/test/java/com/byow/wallet/byow/gui/ReceivingBlockTest.groovy
@@ -0,0 +1,76 @@
+package com.byow.wallet.byow.gui
+
+import javafx.scene.control.TableView
+import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
+
+import static java.util.concurrent.TimeUnit.SECONDS
+import static org.testfx.util.WaitForAsyncUtils.waitFor
+
+class ReceivingBlockTest extends GuiTest {
+    def setup() {
+        loadWalletAndAddBalance()
+    }
+
+    def "should receive bitcoin and add #confirmations confirmations to received address"() {
+        when:
+            clickOn("New")
+            clickOn("Wallet")
+            clickOn("#name")
+            write("My Test Wallet 5")
+            clickOn("Create")
+            def mnemonicSeed = lookup("#mnemonicSeed").queryAs(TextArea).text
+            clickOn("OK")
+            clickOn("Receive")
+            String address = lookup("#receivingAddress").queryAs(TextField).text
+            sleep(TIMEOUT, SECONDS)
+            sendBitcoinAndWait(address)
+            mineBlocks(confirmations)
+            TableView tableView = lookup("#addressesTable").queryAs(TableView)
+        then:
+            tableView.items.size() == 1
+            tableView.items[0].confirmations == confirmations
+            addressIsValid(address, mnemonicSeed, 0)
+        where:
+            confirmations   |   x
+            1               |   null
+            2               |   null
+            3               |   null
+    }
+
+    def "should receive bitcoin and consider the transaction with fewer confirmations as the address confirmations"() {
+        when:
+            clickOn("New")
+            clickOn("Wallet")
+            clickOn("#name")
+            write("My Test Wallet 6")
+            clickOn("Create")
+            def mnemonicSeed = lookup("#mnemonicSeed").queryAs(TextArea).text
+            clickOn("OK")
+            clickOn("Receive")
+            String address = lookup("#receivingAddress").queryAs(TextField).text
+            sleep(TIMEOUT, SECONDS)
+            sendBitcoinAndWait(address)
+            mineBlocks(confirmations)
+            sendBitcoinAndWait(address)
+            mineBlocks(confirmations - 1)
+            TableView tableView = lookup("#addressesTable").queryAs(TableView)
+        then:
+            tableView.items.size() == 1
+            tableView.items[0].confirmations == confirmations - 1
+            addressIsValid(address, mnemonicSeed, 0)
+            where:
+            confirmations   |   x
+            2               |   null
+            3               |   null
+    }
+
+    void mineBlocks(int blocks) {
+        String nodeAddress = nodeGetNewAddressClient.getNewAddress(TESTWALLET)
+        nodeGenerateToAddressClient.generateToAddress(TESTWALLET, blocks, nodeAddress)
+        waitFor(TIMEOUT, SECONDS, {
+            TableView tableView = lookup("#addressesTable").queryAs(TableView)
+            return tableView.items[0].confirmations == blocks
+        })
+    }
+}
