0ab4e19348c43a9e7aea3dba956b0b6111162e37
Humberto Cezar <skateforevis@hotmail.com>
2022-04-14 23:24:51 -0300

    send bitcoin part 2 real

--- src/main/java/com/byow/wallet/byow/api/services/TransactionSizeCalculator.java
+++ src/main/java/com/byow/wallet/byow/api/services/TransactionSizeCalculator.java
@@ -12,13 +12,13 @@ public class TransactionSizeCalculator {
     // OVERHEAD
     private static double N_VERSION = 4;
     private static double INPUT_COUNT = 1; // for up to 252 inputs
-    private static double OUTPUT_COUNT = 1; // for up to 252 inputs
+    private static double OUTPUT_COUNT = 1; // for up to 252 outputs
     private static double N_LOCKTIME = 4;
-    private static double SEGWIT_MARKER_AND_FLAG = 0.5; // only for segwit transactions
+    private static double SEGWIT_MARKER_AND_FLAG = 2; // only for transactions spending segwit UTXOs
 
     // INPUT
     private static double OUTPOINT = 36;
-    private static double SCRIPT_SIG_LENGTH = 1; // for up to 252 vBytes
+    private static double SCRIPT_SIG_LENGTH = 1; // for up to 252 vbytes
     private static double SCRIPT_SIG = 0; // for segwit inputs
     private static double N_SEQUENCE = 4;
     private static double WITNESS_COUNT = 1; // for up to 252 items
@@ -26,21 +26,22 @@ public class TransactionSizeCalculator {
 
     // OUTPUT
     private static double N_VALUE = 8;
-    private static double SCRIPT_PUBKEY_LENGTH = 1; // for up to 252 vBytes
+    private static double SCRIPT_PUBKEY_LENGTH = 1; // for up to 252 vbytes
     private static double SCRIPT_PUBKEY = 22; // for P2WPKH outputs
 
-
     public BigInteger calculate(List<String> inputAddresses, List<String> outputAddresses) {
-        double overhead = N_VERSION + INPUT_COUNT + OUTPUT_COUNT + N_LOCKTIME + SEGWIT_MARKER_AND_FLAG;
+        double overhead = N_VERSION + INPUT_COUNT + OUTPUT_COUNT + N_LOCKTIME + (SEGWIT_MARKER_AND_FLAG / 4);
 
         double input = OUTPOINT + SCRIPT_SIG_LENGTH + SCRIPT_SIG + N_SEQUENCE;
+        double allInputs = inputAddresses.size() * input;
+
         double witness = (WITNESS_COUNT + WITNESS_ITEMS) / 4;
-        double inputsAndWitnesses = inputAddresses.size() * (input + witness);
+        double allWitnesses = inputAddresses.size() * witness;
 
         double output = N_VALUE + SCRIPT_PUBKEY_LENGTH + SCRIPT_PUBKEY;
         double allOutputs = outputAddresses.size() * output;
 
-        BigDecimal result = BigDecimal.valueOf(overhead + inputsAndWitnesses + allOutputs);
+        BigDecimal result = BigDecimal.valueOf(overhead + allInputs + allWitnesses + allOutputs);
         result = result.setScale(0, RoundingMode.HALF_UP);
         return result.toBigInteger();
     }
