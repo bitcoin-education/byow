90770384e09dc614c1996883057f60bcdfbba438
Humberto Cezar <skateforevis@hotmail.com>
2022-05-06 22:44:33 -0300

    send bitcoin part 8

--- src/main/java/com/byow/wallet/byow/gui/services/SendTransactionService.java
+++ src/main/java/com/byow/wallet/byow/gui/services/SendTransactionService.java
@@ -5,8 +5,6 @@ import com.byow.wallet.byow.api.services.node.client.NodeSendRawTransactionClien
 import com.byow.wallet.byow.domains.*;
 import com.byow.wallet.byow.gui.events.TransactionSentEvent;
 import com.byow.wallet.byow.observables.CurrentWallet;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
@@ -18,36 +16,34 @@ import java.util.concurrent.Future;
 
 @Service
 public class SendTransactionService {
-    private static final Logger logger = LoggerFactory.getLogger(SendTransactionService.class);
-
     private final CurrentWallet currentWallet;
 
     private final TransactionSignerService transactionSignerService;
-
-    private final List<AddressConfig> addressConfigs;
 
     private final NodeSendRawTransactionClient nodeSendRawTransactionClient;
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
+    private final UtxoDtoBuilder utxoDtoBuilder;
+
     public SendTransactionService(
         CurrentWallet currentWallet,
         TransactionSignerService transactionSignerService,
-        List<AddressConfig> addressConfigs,
         NodeSendRawTransactionClient nodeSendRawTransactionClient,
-        ApplicationEventPublisher applicationEventPublisher
+        ApplicationEventPublisher applicationEventPublisher,
+        UtxoDtoBuilder utxoDtoBuilder
     ) {
         this.currentWallet = currentWallet;
         this.transactionSignerService = transactionSignerService;
-        this.addressConfigs = addressConfigs;
         this.nodeSendRawTransactionClient = nodeSendRawTransactionClient;
         this.applicationEventPublisher = applicationEventPublisher;
+        this.utxoDtoBuilder = utxoDtoBuilder;
     }
 
     @Async("defaultExecutorService")
     public Future<Void> signAndSend(TransactionDto transactionDto, String password) {
         List<UtxoDto> utxoDtos = transactionDto.selectedUtxos().stream()
-            .map(this::buildUtxoDto)
+            .map(utxoDtoBuilder::build)
             .toList();
         transactionSignerService.sign(transactionDto.transaction(), currentWallet.getMnemonicSeed(), password, utxoDtos);
         try {
@@ -59,20 +55,4 @@ public class SendTransactionService {
         return new AsyncResult<>(null);
     }
 
-
-    private UtxoDto buildUtxoDto(Utxo utxo) {
-        Long addressIndex = currentWallet.getAddress(utxo.address()).getIndex();
-        AddressType addressType = currentWallet.getAddress(utxo.address()).getAddressType();
-        String derivationPath = buildDerivationPath(addressType, addressIndex);
-        return new UtxoDto(derivationPath, utxo.amount());
-    }
-
-    private String buildDerivationPath(AddressType addressType, Long addressIndex) {
-        return addressConfigs.stream()
-            .filter(addressConfig -> addressConfig.addressType().equals(addressType))
-            .findFirst()
-            .get()
-            .derivationPath()
-            .concat("/".concat(addressIndex.toString()));
-    }
 }
