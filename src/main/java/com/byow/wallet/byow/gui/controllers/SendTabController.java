f3160e515ebf9b00c34903641ae5d67a2b1de85b
Humberto Cezar <skateforevis@hotmail.com>
2022-11-18 13:51:48 -0300

    Squashed commit of the following:
    commit 2185ff616371a8d5a038df02fce12d9c2968d0f9
    Author: Humberto Cezar <skateforevis@hotmail.com>
    Date:   Thu May 5 20:46:00 2022 -0300
    
        send bitcoin part 6 real
    
    commit 04054a7c3218f3160c349e2faa3994fc4fc526b1
    Author: Humberto Cezar <skateforevis@hotmail.com>
    Date:   Thu May 5 20:27:58 2022 -0300
    
        send bitcoin part 7 real

--- src/main/java/com/byow/wallet/byow/gui/controllers/SendTabController.java
+++ src/main/java/com/byow/wallet/byow/gui/controllers/SendTabController.java
@@ -1,25 +1,40 @@
 package com.byow.wallet.byow.gui.controllers;
 
+import com.byow.wallet.byow.domains.TransactionDto;
+import com.byow.wallet.byow.gui.services.CreateTransactionService;
 import com.byow.wallet.byow.observables.CurrentWallet;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @Component
 public class SendTabController extends Tab {
     private final CurrentWallet currentWallet;
 
+    @FXML
+    private TextField addressToSend;
+
+    @FXML
+    private TextField amountToSend;
+
+    private final CreateTransactionService createTransactionService;
+
     public SendTabController(
         @Value("fxml/send_tab.fxml") Resource fxml,
         ApplicationContext context,
-        CurrentWallet currentWallet
+        CurrentWallet currentWallet,
+        CreateTransactionService createTransactionService
     ) throws IOException {
         this.currentWallet = currentWallet;
+        this.createTransactionService = createTransactionService;
         FXMLLoader fxmlLoader = new FXMLLoader(
             fxml.getURL(),
             null,
@@ -29,5 +44,17 @@ public class SendTabController extends Tab {
         fxmlLoader.setController(this);
         fxmlLoader.setRoot(this);
         fxmlLoader.load();
+    }
+
+    public void send() {
+        BigDecimal amount = new BigDecimal(amountToSend.getText());
+        TransactionDto transactionDto = createTransactionService.create(addressToSend.getText(), amount);
+        openDialog(transactionDto);
+        addressToSend.clear();
+        amountToSend.clear();
+    }
+
+    private void openDialog(TransactionDto transactionDto) {
+
     }
 }
